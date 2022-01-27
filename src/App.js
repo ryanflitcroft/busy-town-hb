2a801652341fc8eed114ae2eeed774c392a58f0f
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-26 17:11:10 -0800

    comment cleanup

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import VehicleList from './VehicleList';
 import TrafficLight from './TrafficLight';
 
 function App() {
-  // track the following state with a few useState hooks:
-  // lightColor should be a string that starts out as 'red'
   const [lightColor, setLightColor] = useState('red');
-  // lizardSize should be a number that starts out as 10
   const [lizardSize, setLizardSize] = useState(10);
-  // alienSize should be a number that starts out as 10
   const [alienSize, setAlienSize] = useState(10);
-  // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
   const [traffic, setTraffic] = useState(['car', 'truck']);
 
   function handleIncrementLizardWidth() {
@@ -40,56 +35,36 @@ function App() {
 
       <div className="fight">
         <div className="monster">
-          {/* the width of the alien should be ten times whatever the alien size is in state */}
           <img src="alien.png" width={(alienSize * 10)} />
           <div className='buttons'>
-            {/* when you click this button, the alien's size in state should go up by one */}
             <button onClick={handleIncrementAlienWidth}>Oh no! The alien is gobbling up all the electricity!</button>
-            {/* when you click this button, the lizard's size in state should go down by one */}
             <button onClick={handleDecrementLizardWidth}>Amazing! The alien zapped the lizard!</button>
           </div>
         </div>
         
         <div className="monster">
-          {/* the width of the lizard should be ten times whatever the alien size is in state */}
           <img src="lizard.png" width={(lizardSize * 10)} />
           <div className="buttons">
-            {/* when you click this button, the lizard's size in state should go up by one */}
             <button onClick={handleIncrementLizardWidth}>Yegads! The lizard is ramping up to its final form!</button>
-            {/* when you click this button, the alien's size in state should go up by one */}
             <button onClick={handleDecrementAlienWidth}>Oh my! The lizard chomped down on the alien!</button>
           </div>
         </div>
       </div>
-
+      
       <TrafficLight color={lightColor} />
       <div className="buttons">
-        {/* when you click this button, the color of the light in state should be set to 'red' */}
         <button onClick={() => setLightColor('red')}>Red</button>
-        {/* when you click this button, the color of the light in state should be set to 'yellow' */}
         <button onClick={() => setLightColor('yellow')}>Yellow</button>
-        {/* when you click this button, the color of the light in state should be set to 'green' */}
         <button onClick={() => setLightColor('green')}>Green</button>
       </div>
 
-      {/* 
-      the VehicleList component takes in one prop: vehicles.
-      This prop should be an array of strings like ['car', 'truck', 'truck', 'car', 'bus'].
-      Do you have something like that in state that you could pass as a vehicles prop? 
-      */}
       <VehicleList vehicles={traffic} />
       <div className='buttons'>
-        {/* This part is weird */}
-        {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'car' to the end */}
         <button onClick={() => handleTraffic('car')}>Car</button>
-        {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'bus' to the end */}
         <button onClick={() => handleTraffic('bus')}>Bus</button>
-        {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'truck' to the end */}
         <button onClick={() => handleTraffic('truck')}>Truck</button>
-        {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'motorcycle' to the end */}
         <button onClick={() => handleTraffic('motorcycle')}>Motorcycle</button>
       </div>
-
     </div>
   );
 }
--- src/VehicleList.js
+++ src/VehicleList.js
@@ -2,12 +2,9 @@ import React from 'react';
 import Vehicle from './Vehicle';
 
 export default function VehicleList(props) {
-  console.log('||vehicles: ' + props.vehicles);
+
   return (
     <div className="traffic">
-      {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
-      {/* map through the vehicles array in props */}
-      {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
       {props.vehicles.map((vehicle, i) => 
         <Vehicle key={vehicle + i} vehicle={vehicle} />
       )}
